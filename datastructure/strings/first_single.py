32a67a7a5709b2e1319c247cd52c7f8ee5bcc15b
hbnnlong@163.com <393993705@qq.com>
2016-04-21 12:34:34 +0800

    add find_first_single string

--- datastructure/strings/first_single.py
+++ datastructure/strings/first_single.py
@@ -1,41 +1,16 @@
 import sys
 
-class First_Single:
-
-    def __init__(self,obj):
-        if isinstance(obj,basestring):
-            self.obj = list(obj)
-        elif isinstance(obj,(tuple,list)):
-            self.obj = obj
+def find_1st_single(str):
+    hash_dict = dict.fromkeys(str,None)
+    for index,item in enumerate(str):
+        if hash_dict[item] is None:
+            hash_dict[item] = index
         else:
-            print 'incorrect data type'
-            sys.exit(1)
-        self._create_dict()
-
-    def _create_dict(self):
-        self.obj_in_dict = dict.fromkeys(self.obj,-1)
-        
-    def insert(self):
-        for index,char in enumerate(self.obj):
-            if self.obj_in_dict[char] == -1:
-                self.obj_in_dict[char] = index
-            elif self.obj_in_dict[char] >= 0:
-                self.obj_in_dict[char] = -2
-
-    def get_first_single(self):
-        items = self.obj_in_dict.items()
-        result = filter(lambda x:x[1] >= 0,items)
-        return sorted(result,key = lambda x:x[1])[0][0]
+            hash_dict[item] = -1
+    result = filter(lambda x:x[1]>=0,hash_dict.iteritems())
+    print sorted(result,key=lambda x:x[1]).pop(0)
 
 
-
-
-
-if __name__ == "__main__":
-
-    string = 'igoogleilae'
-    first = First_Single(string)
-    first.insert()
-    print 'the first word which has only one is %s'%first.get_first_single()
+find_1st_single('wfwfdfc')
 
 
